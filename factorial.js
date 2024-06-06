9956dddcd43dfb0d25957add37d3ae98d2202cce
ravenjoswell <rjc.panlaqui@gmail.com>
2024-06-05 18:35:50 -1000

    factorial function

--- factorial.js
+++ factorial.js
@@ -1,5 +1,17 @@
 function factorial(num) {
-  return 0;
+  let finalNum = 1
+  for(let i = 0; i > 0; i--){
+    finalNum = finalNum * 1
+  }
+  return finalNum
 }
 
-module.exports = factorial;
+console.log(factorial(0) === 1);
+console.log(factorial(1) === 1);
+console.log(factorial(2) === 2);
+console.log(factorial(4) === 24);
+console.log(factorial(8) === 40320);
+console.log(factorial(18) === 6402373705728000);
+
+
+// module.exports = factorial;
